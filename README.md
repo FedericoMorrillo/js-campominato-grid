9c0303a2344d9f2c2fb32e565f99846078a3457b
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-15 16:44:54 +0100

    feat: generazione celle on click

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 2) inseriamo nell' header il logo e un bottone;
 3) creiamo un contenitore;
 4) creiamo la classe cella tramite il css e gli inseriamo delle proprietà;
-5) creiamo una funzione che genera per noi le celle al click del button;
+5) creiamo una funzione che attraverso un ciclo genera per noi le celle al click del button;
 6) al click sulla cella cambia colore e stampa in console il numero della cella;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'use strict'
+//funzioni
+function cellsGenerator(tag, classname, content){
+        const cella = document.createElement(tag);
+        cella.classList.add(classname);
+        cella.append(content);
+        return cella;
+}
+//funzioni
+
+//dichiarazioni
+const board = document.querySelector('.board')
+const buttonPlay = document.querySelector('.btn')
+
+//scateniamo un evento on click sul bottone
+buttonPlay.addEventListener('click', function(){
+
+    //elimina le celle se precedentemente ho cliccato sul bottone
+    board.innerHTML= '';
+
+    //ciclo per creare le celle
+   for(let i=1; i<=100; i++){
+    const celle = cellsGenerator('div', 'cells', i);
+    board.append(celle);
+   }
+
+})
