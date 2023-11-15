1213e49a9e90430becf5086e40764d5a75accadf
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-15 20:42:33 +0100

    feat: difficoltà aggiunte

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 4) creiamo la classe cella tramite il css e gli inseriamo delle proprietà;
 5) creiamo una funzione che attraverso un ciclo genera per noi le celle al click del button;
 6) nel ciclo aggiungiamo un evento on click sulla cella cambiando colore e stampando in console il numero di quest' ultima;
+7) aggiungiamo un input di tipo select con all' interno le tre difficoltà;
+8) prendiamo l' id dell' input e salviamolo in una variabile in js;
+9) aggiungiamo nel css le classi di difficoltà con le rispettive misure per farle entrare nel contenitore perfettamente;
+9) nell' evento on click del bottone creiamo 3 condizioni con all' interno il ciclo che genera le celle + aggiunge la classe di difficoltà;
+
--- js/main.js
+++ js/main.js
@@ -1,34 +1,68 @@
-'use strict'
+"use strict";
 //funzioni
-function cellsGenerator(tag, classname, content){
-        const cella = document.createElement(tag);
-        cella.classList.add(classname);
-        cella.append(content);
-        return cella;
+function cellsGenerator(tag, classname, content) {
+  const cella = document.createElement(tag);
+  cella.classList.add(classname);
+  cella.append(content);
+  return cella;
 }
 //funzioni
 
 //dichiarazioni
-const board = document.querySelector('.board')
-const buttonPlay = document.querySelector('.btn')
+const board = document.querySelector(".board");
+const buttonPlay = document.querySelector(".btn");
+let lvDifficolta = document.getElementById("difficolta");
 
 //scateniamo un evento on click sul bottone
-buttonPlay.addEventListener('click', function(){
+buttonPlay.addEventListener("click", function () {
+  //elimina le celle se precedentemente ho cliccato sul bottone
+  board.innerHTML = "";
 
-    //elimina le celle se precedentemente ho cliccato sul bottone
-    board.innerHTML= '';
+  //condizione
+  if (lvDifficolta.value === "facile") {
+    //ciclo per creare celle
+    for (let i = 1; i <= 100; i++) {
+      const celle = cellsGenerator("div", "cells", i);
+      celle.classList.add("cells-hard");
+      board.append(celle);
 
-    //ciclo per creare le celle
-   for(let i=1; i<=100; i++){
-    const celle = cellsGenerator('div', 'cells', i);
-    board.append(celle);
+       //aggiunge un evento on click sulle celle
+       celle.addEventListener("click", function () {
+        this.classList.add("select-cell");
+        console.log(i);
+      });
+    }
+  }
 
-    //aggiunge un evento on click sulle celle
-    celle.addEventListener('click', function(){
-    this.classList.add('select-cell');
-    console.log(i);
-    })
-   }
-    
-})
-    
+  //condizione
+  else if (lvDifficolta.value === "media") {
+    //ciclo per creare celle
+    for (let i = 1; i <= 81; i++) {
+      const celle = cellsGenerator("div", "cells", i);
+      celle.classList.add("cells-medium");
+      board.append(celle);
+
+      //aggiunge un evento on click sulle celle
+      celle.addEventListener("click", function () {
+        this.classList.add("select-cell");
+        console.log(i);
+      });
+    }
+  }
+
+  //condizione
+  else if (lvDifficolta.value === "difficile") {
+    //ciclo per creare celle
+    for (let i = 1; i <= 49; i++) {
+      const celle = cellsGenerator("div", "cells", i);
+      celle.classList.add("cells-easy");
+      board.append(celle);
+
+      //aggiunge un evento on click sulle celle
+      celle.addEventListener("click", function () {
+        this.classList.add("select-cell");
+        console.log(i);
+      });
+    }
+  }
+});
